e47f05a48f0e44ac8da9d5608eb05a588bf454db
Sydney <sydney3@stanford.edu>
2025-04-24 19:21:41 +0200

    ifu kinematics

---
+++ Modeling/Kinematics/galkin_ifu_sherlock_script.py
@@ -0,0 +1,90 @@
+import h5py
+import argparse
+import pandas as pd
+import numpy as np
+from filelock import FileLock
+import galkin_utils # requires lenstronomy
+
+"""
+python3 galkin_sherlock_script.py --lens_idx=1 
+    --h5_posteriors_path=/scratch/users/sydney3/forecast/darkenergy-from-LAGN/Experiments/lsst_forecast/DataVectors/gold/quad_posteriors.h5
+"""
+    
+def main(args):
+    """ Main function for script. """
+
+    # get args
+    LENS_IDX = args.lens_idx
+    h5_posteriors_file = args.h5_posteriors_path
+    
+    #('/scratch/users/sydney3/forecast/darkenergy-from-LAGN/'+
+    #    'Experiments/lsst_forecast/DataVectors/gold/quad_posteriors.h5')
+    # NOTE: hardcoded for gold right now...
+    truth_df = pd.read_csv('/scratch/users/sydney3/forecast/darkenergy-from-LAGN/'+
+        'Experiments/lsst_forecast/DataVectors/gold/truth_metadata.csv')
+
+    # read in fpd samps, lens_param_samps, beta_ani_samps
+    with FileLock(h5_posteriors_file + ".lock"):
+
+        h5 = h5py.File(h5_posteriors_file, 'r')
+        fpd_samps = h5['fpd_samps'][:]
+        lens_param_samps = h5['lens_param_samps'][:]
+        beta_ani_samps = h5['beta_ani_samps'][:]
+        catalog_idxs = h5['catalog_idxs'][:]
+        h5.close()
+
+    # compute kinematics
+    n_fpd_samps = np.shape(fpd_samps)[1]
+    MUSE_c_sqrtJ_samps = np.empty((n_fpd_samps,3))
+    NIRSPEC_c_sqrtJ_samps = np.empty((n_fpd_samps,10))
+    catalog_idx = catalog_idxs[LENS_IDX]
+    R_sersic_truth = truth_df.loc[truth_df['catalog_idx']==catalog_idx,
+        'lens_light_parameters_R_sersic'].item()
+    n_sersic_truth = truth_df.loc[truth_df['catalog_idx']==catalog_idx,
+        'lens_light_parameters_n_sersic'].item()
+
+    # compute samples one by one
+    for fp_idx in range(0,n_fpd_samps):
+
+        # MUSE
+        muse_csqrtJ = galkin_utils.ground_truth_ifu_c_sqrtJ(
+            galkin_utils.kinematicsAPI_MUSE,
+            theta_E=lens_param_samps[LENS_IDX,fp_idx,0],
+            gamma_lens=lens_param_samps[LENS_IDX,fp_idx,3],
+            R_sersic=R_sersic_truth,n_sersic=n_sersic_truth,
+            beta_ani=beta_ani_samps[LENS_IDX,fp_idx])
+        MUSE_c_sqrtJ_samps[fp_idx] = muse_csqrtJ
+
+        # NIRSPEC
+        nirspec_csqrtJ = galkin_utils.ground_truth_ifu_c_sqrtJ(
+            galkin_utils.kinematicsAPI_NIRSPEC,
+            theta_E=lens_param_samps[LENS_IDX,fp_idx,0],
+            gamma_lens=lens_param_samps[LENS_IDX,fp_idx,3],
+            R_sersic=R_sersic_truth,n_sersic=n_sersic_truth,
+            beta_ani=beta_ani_samps[LENS_IDX,fp_idx])
+        NIRSPEC_c_sqrtJ_samps[fp_idx] = nirspec_csqrtJ
+
+    # filelock & write kinematics...
+    with FileLock(h5_posteriors_file + ".lock"):
+        h5 = h5py.File(h5_posteriors_file, 'r+')
+        # TODO fix this!!!
+        h5['MUSE_c_sqrtJ_samps'][LENS_IDX, ...] = c_sqrtJ_samps
+        h5.close()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description="Computes c_sqrtJ samples for one lens"
+    )
+    parser.add_argument(
+        "--lens_idx",
+        type=int,
+        default=0,
+        help=("Lens index in posteriors.h5 file (not catalog_idx!)")
+    )
+    parser.add_argument(
+        "--h5_posteriors_path",
+        type=str)
+
+    args = parser.parse_args()
+    main(args)
--- Modeling/Kinematics/galkin_utils.py
+++ Modeling/Kinematics/galkin_utils.py
@@ -7,43 +7,133 @@ import sys
 sys.path.insert(0, '/scratch/users/sydney3/forecast/darkenergy-from-LAGN/')
 import Utils.tdc_utils as tdc_utils
 
-# hard-coded assumptions for 4MOST
+##################
+# 4MOST Kinematics
+##################
 R_APERTURE = 0.725
 PSF_FWHM = 0.5
 
-kwargs_aperture = {
-    'aperture_type': 'shell', 
-    'r_in': 0., 
-    'r_out': R_APERTURE,
-    'center_ra': 0, 'center_dec': 0}
+def setup_4MOST_kinematics():
 
-kwargs_seeing = {'psf_type': 'GAUSSIAN', 'fwhm': PSF_FWHM}
+    kwargs_aperture = {
+        'aperture_type': 'shell', 
+        'r_in': 0., 
+        'r_out': R_APERTURE,
+        'center_ra': 0, 'center_dec': 0}
 
-kwargs_numerics_galkin = { 
-    'interpol_grid_num': 1000,  # numerical interpolation, should converge -> infinity
-    'log_integration': True,  # log or linear interpolation of surface brightness and mass models
-    'max_integrate': 100, 'min_integrate': 0.001}  # lower/upper bound of numerical integrals
+    kwargs_seeing = {'psf_type': 'GAUSSIAN', 'fwhm': PSF_FWHM}
 
-kwargs_model = {
-    'lens_model_list':['SPP'],
-    'lens_light_model_list':['SERSIC']
-}
+    kwargs_numerics_galkin = { 
+        'interpol_grid_num': 1000,  # numerical interpolation, should converge -> infinity
+        'log_integration': True,  # log or linear interpolation of surface brightness and mass models
+        'max_integrate': 100, 'min_integrate': 0.001}  # lower/upper bound of numerical integrals
 
-anisotropy_model = 'const'
+    kwargs_model = {
+        'lens_model_list':['SPP'],
+        'lens_light_model_list':['SERSIC']
+    }
 
-kinematicsAPI = KinematicsAPI(0.5, 2., kwargs_model, 
-    kwargs_aperture, kwargs_seeing, anisotropy_model, 
-    kwargs_numerics_galkin=kwargs_numerics_galkin, 
-    lens_model_kinematics_bool=[True, False],
-    sampling_number=5000,MGE_light=True)
+    anisotropy_model = 'const'
 
+    kinematicsAPI = KinematicsAPI(0.5, 2., kwargs_model, 
+        kwargs_aperture, kwargs_seeing, anisotropy_model, 
+        kwargs_numerics_galkin=kwargs_numerics_galkin, 
+        lens_model_kinematics_bool=[True, False],
+        sampling_number=5000,MGE_light=True)
+    
+    return kinematicsAPI
+
+kinematicsAPI_4MOST = setup_4MOST_kinematics()
+
+##################
+# MUSE Kinematics
+##################
+MUSE_R_BINS = [0.,0.5,1.,1.5] #arcsec
+MUSE_PSF_FWHM = 0.5
+
+def setup_MUSE_kinematics():
+
+    kwargs_aperture = {
+        'aperture_type': 'IFU_shells', 
+        'r_bins': MUSE_R_BINS,
+        'center_ra': 0, 'center_dec': 0}
+
+    kwargs_seeing = {'psf_type': 'GAUSSIAN', 'fwhm': MUSE_PSF_FWHM}
+
+    kwargs_numerics_galkin = { 
+        'interpol_grid_num': 1000,  # numerical interpolation, should converge -> infinity
+        'log_integration': True,  # log or linear interpolation of surface brightness and mass models
+        'max_integrate': 100, 'min_integrate': 0.001}  # lower/upper bound of numerical integrals
+
+    kwargs_model = {
+        'lens_model_list':['SPP'],
+        'lens_light_model_list':['SERSIC']
+    }
+
+    anisotropy_model = 'const'
+
+    kinematicsAPI = KinematicsAPI(0.5, 2., kwargs_model, 
+        kwargs_aperture, kwargs_seeing, anisotropy_model, 
+        kwargs_numerics_galkin=kwargs_numerics_galkin, 
+        lens_model_kinematics_bool=[True, False],
+        sampling_number=5000,MGE_light=True)
+    
+    return kinematicsAPI
+
+kinematicsAPI_MUSE = setup_MUSE_kinematics()
+
+
+#########################
+# JWST NIRSPEC Kinematics
+#########################
+JWSTNIRSPEC_R_BINS = [0.,0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0] #arcsec
+JWSTNIRSPEC_PSF_FWHM = 0.05
+
+# calcium III at redshift 0.5, take wavelength in angstrom, compute diffraction limit with D=6.5M
+# 0.05 arcsec
+# also see this paper that says 0.08 arcsec https://www.aanda.org/articles/aa/pdf/2022/05/aa42663-21.pdf
+def setup_NIRSPEC_kinematics():
+
+    kwargs_aperture = {
+        'aperture_type': 'IFU_shells', 
+        'r_bins': JWSTNIRSPEC_R_BINS,
+        'center_ra': 0, 'center_dec': 0}
+
+    kwargs_seeing = {'psf_type': 'GAUSSIAN', 'fwhm': JWSTNIRSPEC_PSF_FWHM}
+
+    kwargs_numerics_galkin = { 
+        'interpol_grid_num': 1000,  # numerical interpolation, should converge -> infinity
+        'log_integration': True,  # log or linear interpolation of surface brightness and mass models
+        'max_integrate': 100, 'min_integrate': 0.001}  # lower/upper bound of numerical integrals
+
+    kwargs_model = {
+        'lens_model_list':['SPP'],
+        'lens_light_model_list':['SERSIC']
+    }
+
+    anisotropy_model = 'const'
+
+    kinematicsAPI = KinematicsAPI(0.5, 2., kwargs_model, 
+        kwargs_aperture, kwargs_seeing, anisotropy_model, 
+        kwargs_numerics_galkin=kwargs_numerics_galkin, 
+        lens_model_kinematics_bool=[True, False],
+        sampling_number=5000,MGE_light=True)
+    
+    return kinematicsAPI
+
+kinematicsAPI_NIRSPEC = setup_NIRSPEC_kinematics()
+
+
+#####################
+# 4MOST APERTURE KIN
+#####################
 
 def ground_truth_veldisp(theta_E,gamma_lens,R_sersic,n_sersic,
     z_lens,z_src,gt_cosmo_astropy):
 
     default_distance_ratio = np.sqrt(
-        kinematicsAPI._kwargs_cosmo['d_s'] / 
-        kinematicsAPI._kwargs_cosmo['d_ds'])
+        kinematicsAPI_4MOST._kwargs_cosmo['d_s'] / 
+        kinematicsAPI_4MOST._kwargs_cosmo['d_ds'])
 
     kwargs_anisotropy = {'beta': 0.}
 
@@ -62,7 +152,7 @@ def ground_truth_veldisp(theta_E,gamma_lens,R_sersic,n_sersic,
         'center_y': 0.,
     }]
 
-    vel_disp_numerical = kinematicsAPI.velocity_dispersion(kwargs_lens, 
+    vel_disp_numerical = kinematicsAPI_4MOST.velocity_dispersion(kwargs_lens, 
         kwargs_lens_light, kwargs_anisotropy, r_eff=R_sersic, theta_E=theta_E)
     
     corrected_distance_ratio = np.sqrt(
@@ -76,8 +166,8 @@ def ground_truth_veldisp(theta_E,gamma_lens,R_sersic,n_sersic,
 def ground_truth_c_sqrtJ(theta_E,gamma_lens,R_sersic,n_sersic,beta_ani):
 
     default_distance_ratio = np.sqrt(
-        kinematicsAPI._kwargs_cosmo['d_s'] / 
-        kinematicsAPI._kwargs_cosmo['d_ds'])
+        kinematicsAPI_4MOST._kwargs_cosmo['d_s'] / 
+        kinematicsAPI_4MOST._kwargs_cosmo['d_ds'])
 
     kwargs_anisotropy = {'beta': beta_ani}
 
@@ -96,7 +186,93 @@ def ground_truth_c_sqrtJ(theta_E,gamma_lens,R_sersic,n_sersic,beta_ani):
         'center_y': 0.,
     }]
 
-    vel_disp_numerical = kinematicsAPI.velocity_dispersion(kwargs_lens, 
+    vel_disp_numerical = kinematicsAPI_4MOST.velocity_dispersion(kwargs_lens, 
+        kwargs_lens_light, kwargs_anisotropy, r_eff=R_sersic, theta_E=theta_E)
+
+    c_sqrtJ = vel_disp_numerical / default_distance_ratio
+
+    return c_sqrtJ
+
+
+
+#######################
+# IFU KIN (muse/nirspec)
+#######################
+
+def ground_truth_ifu_vdisp(ifu_api,theta_E,gamma_lens,R_sersic,n_sersic,
+    z_lens,z_src,gt_cosmo_astropy):
+    """
+    Args:
+        ifu_api (kinematicsAPI_MUSE or kinematicsAPI_NIRSPEC)
+    Returns:
+        [v_disp] size=(3,) for muse
+        [v_disp] size=(10,) for nirspec
+    """
+
+    default_distance_ratio = np.sqrt(
+            ifu_api._kwargs_cosmo['d_s'] / 
+            ifu_api._kwargs_cosmo['d_ds'])
+
+    kwargs_anisotropy = {'beta': 0.}
+
+    kwargs_lens = [{
+        'theta_E':theta_E, 
+        'gamma':gamma_lens, 
+        "center_x":0., 
+        "center_y":0.
+    }]
+
+    kwargs_lens_light = [{
+        'amp': 10.,
+        'R_sersic': R_sersic,
+        'n_sersic': n_sersic,
+        'center_x': 0.,
+        'center_y': 0.,
+    }]
+
+    vel_disp_numerical = ifu_api.velocity_dispersion_map(kwargs_lens, 
+        kwargs_lens_light, kwargs_anisotropy, r_eff=R_sersic, theta_E=theta_E)
+
+    corrected_distance_ratio = np.sqrt(
+        tdc_utils.kin_distance_ratio(gt_cosmo_astropy,z_lens,z_src))
+
+    vel_disp_numerical *= (corrected_distance_ratio/default_distance_ratio)
+
+    return vel_disp_numerical
+
+
+def ground_truth_ifu_c_sqrtJ(ifu_api,theta_E,gamma_lens,R_sersic,n_sersic,
+    beta_ani):
+    """
+    Args:
+        ifu_api (kinematicsAPI_MUSE or kinematicsAPI_NIRSPEC)
+    Returns:
+        [c_sqrtJ] size=(3,) for muse
+        [c_sqrtJ] size=(10,) for nirspec
+    """
+
+    default_distance_ratio = np.sqrt(
+        ifu_api._kwargs_cosmo['d_s'] / 
+        ifu_api._kwargs_cosmo['d_ds'])
+
+    kwargs_anisotropy = {'beta': beta_ani}
+
+    kwargs_lens = [{
+        'theta_E':theta_E, 
+        'gamma':gamma_lens, 
+        "center_x":0., 
+        "center_y":0.
+    }]
+
+    kwargs_lens_light = [{
+        'amp': 10.,
+        'R_sersic': R_sersic,
+        'n_sersic': n_sersic,
+        'center_x': 0.,
+        'center_y': 0.,
+    }]
+
+    vel_disp_numerical = ifu_api.velocity_dispersion_map(kwargs_lens, 
         kwargs_lens_light, kwargs_anisotropy, r_eff=R_sersic, theta_E=theta_E)
 
     c_sqrtJ = vel_disp_numerical / default_distance_ratio
