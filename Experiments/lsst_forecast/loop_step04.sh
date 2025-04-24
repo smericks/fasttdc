1c8c21b3b62fc14c1ebfabc1f69154f266b84cde
Sydney Megan Erickson <sydney3@sh03-ln07.stanford.edu>
2025-04-24 10:18:04 -0700

    sherlock kinematics

---
+++ Experiments/lsst_forecast/loop_step04.sh
@@ -0,0 +1,4 @@
+for (( i=300; i<=399; ++i))
+do
+	sbatch step04_kinematics.sh $i
+done
---
+++ Experiments/lsst_forecast/step04_kinematics.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -J gen_kin
+#SBATCH -p kipac,normal,hns
+#SBATCH -c 4
+#SBATCH -N 1
+#SBATCH -t 00:90:00
+#SBATCH --mem 8000
+#SBATCH --error /scratch/users/sydney3/slurm_out/gen_kin_%j.err
+#SBATCH --output /scratch/users/sydney3/slurm_out/gen_kin_%j.out
+
+cd /scratch/users/sydney3
+module load python/3.9
+source venvs/forecast_env/bin/activate
+cd forecast/darkenergy-from-LAGN/Modeling/Kinematics
+# first argument passed to sbatch is the lens idx!
+python3 galkin_sherlock_script.py --lens_idx=$1 --h5_posteriors_path=/scratch/users/sydney3/forecast/darkenergy-from-LAGN/Experiments/lsst_forecast/DataVectors/gold/dbl_posteriors.h5
