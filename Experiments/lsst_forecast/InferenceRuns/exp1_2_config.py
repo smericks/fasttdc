709b65598c045af472463e7cf4cd84fbd6e013a1
Sydney <sydney3@stanford.edu>
2025-06-11 00:28:19 -0700

    test with exp1_2

--- Experiments/lsst_forecast/InferenceRuns/exp1_2_config.py
+++ Experiments/lsst_forecast/InferenceRuns/exp1_2_config.py
@@ -6,7 +6,7 @@ import numpy as np
 from scipy.stats import norm
 
 # random seed
-RANDOM_SEED = 123
+RANDOM_SEED = 1
 
 # file locations
 static_dv_file = 'InferenceRuns/exp1_2/static_datavectors_seed'+str(RANDOM_SEED)+'.json'
@@ -15,12 +15,14 @@ gold_dbls_h5_file = 'DataVectors/gold/dbl_posteriors_KIN.h5'
 gold_metadata_file = 'DataVectors/gold/truth_metadata.csv'
 
 NUM_FPD_SAMPS = 5000
-NUM_MCMC_EPOCHS = 2
-NUM_MCMC_WALKERS = 48
-COSMO_MODEL = 'LCDM_lambda_int_beta_ani'
+NUM_MCMC_EPOCHS = 1000
+NUM_MCMC_WALKERS = 70
+COSMO_MODEL = 'w0waCDM_fullcPDF'
 # NOTE: use norms.csv to read off modeling prior for each model
-GOLD_GAMMA_LENS_PRIOR = norm(loc=2.09,scale=0.16).logpdf # hst_norms.csv: 2.0882867897222503,0.16008433847145742
-SILVER_GAMMA_LENS_PRIOR = norm(loc=2.03,scale=0.19).logpdf # norms2.csv: 2.033213914041585,0.1915982613222065
+mu_lp_gold = [0.85,0.,0.,2.09,0.,0.,0.,0.,0.,0.] # hst_norms.csv
+stddev_lp_gold = [0.28,0.06,0.06,0.16,0.20,0.20,0.06,0.06,0.34,0.34]
+mu_lp_silver = [1.42,0.,0.,2.03,0.,0.,0.,0.,0.,0.]# norms2.csv
+stddev_lp_silver = [0.70,0.1,0.1,0.20,0.20,0.20,0.06,0.06,0.37,0.37]
 BETA_ANI_PRIOR = norm(loc=0.,scale=0.2).logpdf
 BACKEND_PATH = 'InferenceRuns/exp1_2/lcdm_seed'+str(RANDOM_SEED)+'_backend.h5'
 RESET_BACKEND=True
@@ -150,7 +152,8 @@ likelihood_configs = {
         'kin_meas_error_percent':0.05,
         'kin_meas_error_kmpersec':None,
         'num_gaussianized_samps':NUM_FPD_SAMPS,
-        'log_prob_gamma_nu_int':GOLD_GAMMA_LENS_PRIOR,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
         'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
     },
 
@@ -167,7 +170,8 @@ likelihood_configs = {
         'kin_meas_error_percent':0.05,
         'kin_meas_error_kmpersec':None,
         'num_gaussianized_samps':NUM_FPD_SAMPS,
-        'log_prob_gamma_nu_int':GOLD_GAMMA_LENS_PRIOR,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
         'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
     },
 
@@ -183,7 +187,8 @@ likelihood_configs = {
         'kin_meas_error_percent':0.05,
         'kin_meas_error_kmpersec':None,
         'num_gaussianized_samps':NUM_FPD_SAMPS,
-        'log_prob_gamma_nu_int':GOLD_GAMMA_LENS_PRIOR,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
         'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
     },
 
@@ -200,7 +205,8 @@ likelihood_configs = {
         'kin_meas_error_percent':0.05,
         'kin_meas_error_kmpersec':None,
         'num_gaussianized_samps':NUM_FPD_SAMPS,
-        'log_prob_gamma_nu_int':GOLD_GAMMA_LENS_PRIOR,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
         'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
     },
 
@@ -216,7 +222,8 @@ likelihood_configs = {
         'kin_meas_error_percent':0.05,
         'kin_meas_error_kmpersec':None,
         'num_gaussianized_samps':NUM_FPD_SAMPS,
-        'log_prob_gamma_nu_int':GOLD_GAMMA_LENS_PRIOR,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
         'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
     },
 }
